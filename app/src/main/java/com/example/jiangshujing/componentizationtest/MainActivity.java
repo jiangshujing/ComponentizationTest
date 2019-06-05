9c872a46875b55cd5b8f98b4e76d53ee76b12210
jiangshujing <jiangshujing@teddymobile.cn>
2019-06-05 11:02:20 +0800

    添加 lib-base LibraryA LibraryB 依赖，并调用基本库的方法

--- app/src/main/java/com/example/jiangshujing/componentizationtest/MainActivity.java
+++ app/src/main/java/com/example/jiangshujing/componentizationtest/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.jiangshujing.componentizationtest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import com.example.jiangshujing.lib_base.TestBase;
+import com.example.jiangshujing.librarya.TestA;
+import com.example.jiangshujing.libraryb.TestB;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestBase.set("我是 lib_base ");
+        TestA.set("我是 libA");
+        TestB.set("我是 libB");
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(TestA.get();
+
+        Log.e("MainActivity",TestBase.get());
+        Log.e("MainActivity",TestA.get());
+        Log.e("MainActivity",TestB.get());
     }
 }
